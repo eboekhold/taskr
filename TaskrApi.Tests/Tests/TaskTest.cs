815d2b7d2f1eff1bf76ddda04ce2b76ce8a5d2bd
Eva <eva.boekhold@gmail.com>
2026-08-03 18:08:23 +0200

    Add tests for GET api/tasks/{id}

--- TaskrApi.Tests/Tests/TaskTest.cs
+++ TaskrApi.Tests/Tests/TaskTest.cs
@@ -47,6 +47,37 @@ public class TasksApiTests(CustomWebApplicationFactory factory) : IAsyncLifetime
   }
 
   [Fact]
+  public async Task GetTask_WhenOneExists_ReturnsTask()
+  {
+    // Arrange
+    using var scope = factory.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    var existingTask = db.Tasks.Add(new TaskrApi.Models.Task { Name = "Cadeautje kopen" });
+    await db.SaveChangesAsync(TestContext.Current.CancellationToken);
+
+    // Act
+    var response = await factory.HttpClient.GetAsync($"/api/tasks/{existingTask.Entity.Id}", TestContext.Current.CancellationToken);
+
+    // Assert
+    response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+    var task = await response.Content.ReadFromJsonAsync<TaskrApi.Models.Task>(TestContext.Current.CancellationToken);
+    task.Should().NotBeNull();
+    task.Name.Should().Be("Cadeautje kopen");
+  }
+
+  [Fact]
+  public async Task GetTask_WhenNotFound_ReturnsNotFound()
+  {
+    // Act
+    var response = await factory.HttpClient.GetAsync("/api/tasks/1", TestContext.Current.CancellationToken);
+
+    // Assert
+    response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+  }
+
+  [Fact]
   public async Task PutTask_WhenOneExists_ReturnsUpdatedTask()
   {
     // Arrange
