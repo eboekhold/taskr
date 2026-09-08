f066788627f587bad0185e328396af3bc33ba6a3
Eva <eva.boekhold@gmail.com>
2026-09-08 21:13:53 +0200

    Implement checkbox for changing task completion

--- TaskrClient/src/app/task-details/task-details.ts
+++ TaskrClient/src/app/task-details/task-details.ts
@@ -1,4 +1,5 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Task } from '../app';
 
 @Component({
@@ -8,5 +9,16 @@ import { Task } from '../app';
   styleUrl: './task-details.scss',
 })
 export class TaskDetails {
+  private http = inject(HttpClient);
+
   task = input<Task | null>(null);
+
+  onCompletedChanged(event: Event): void {
+    const isChecked = (event.target as HTMLInputElement).checked;
+
+    // Send a PUT request to the API and overwrite the isComplete property.
+    this.http.put(`/api/Tasks/${this.task()?.id}`, { ...this.task(), isComplete: isChecked }).subscribe(
+      // TODO: Do something interesting here, like handling errors and showing a toast message.
+    );
+  }
 }
